40bd2f754845b010744a3013e2eab0ef292258d1
Victor Kovalchuk <vict02r05@gmail.com>
2026-09-22 21:02:26 +0300

    defining & validating custom events

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -41,6 +41,17 @@ export default {
       default: false
     }
   },
+  emits: ['toggle-favorite'],
+  // emits: {
+  //   'toggle-favorite': function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn('Id is missing');
+  //       return false;
+  //     }
+  //   }
+  // },
   data() {
     return {
       detailsAreVisible: false
